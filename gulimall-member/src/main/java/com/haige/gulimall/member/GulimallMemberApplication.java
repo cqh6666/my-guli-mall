cd5745b7072c793bf36e08761d1665bc8d9a1607
alexChan <2018ch@m.scnu.edu.cn>
2021-10-29 19:54:09 +0800

    项目初始结构创建

---
+++ gulimall-member/src/main/java/com/haige/gulimall/member/GulimallMemberApplication.java
@@ -0,0 +1,13 @@
+package com.haige.gulimall.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallMemberApplication.class, args);
+    }
+
+}
